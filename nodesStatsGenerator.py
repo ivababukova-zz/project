02e6bdb7a81c5b19e6de53ab2b44a5378b048656
ivababukova <ibabukova@gmail.com>
2016-02-25 21:36:40 +0000

    Results are from nauru now. Started generating sttistics for the number of nodes used to solve each of the sip problems

---
+++ nodesStatsGenerator.py
@@ -0,0 +1,76 @@
+
+aids0lista = ["resultsSIP0/aids/results0.out", "resultsSIP0/aids/results1.out", "resultsSIP0/aids/results2.out","resultsSIP0/aids/results3.out","resultsSIP0/aids/results4.out","resultsSIP0/aids/results5.out"]
+aids1lista = ["resultsSIP1/aids/results0.out", "resultsSIP1/aids/results1.out", "resultsSIP1/aids/results2.out","resultsSIP1/aids/results3.out","resultsSIP1/aids/results4.out","resultsSIP1/aids/results5.out"]
+
+pcms1lista = ["resultsSIP1/pcms/results16_1BLH.cm.A.out",  "resultsSIP1/pcms/results32_1ATU.cm.A.out", "resultsSIP1/pcms/results32_1D1U.cm.A.out", "resultsSIP1/pcms/results16_1C5G.cm.A.out", "resultsSIP1/pcms/results32_1CNQ.cm.A.out", "resultsSIP1/pcms/results8_1A0G.cm.A.out", "resultsSIP1/pcms/results32_1ARO.cm.L.out", "resultsSIP1/pcms/results32_1CY1.cm.A.out", "resultsSIP1/pcms/results8_1A0G.cm.B.out"]
+pcms0lista = ["resultsSIP0/pcms/results16_1BLH.cm.A.out",  "resultsSIP0/pcms/results32_1ATU.cm.A.out", "resultsSIP0/pcms/results32_1D1U.cm.A.out", "resultsSIP0/pcms/results16_1C5G.cm.A.out", "resultsSIP0/pcms/results32_1CNQ.cm.A.out", "resultsSIP0/pcms/results8_1A0G.cm.A.out", "resultsSIP0/pcms/results32_1ARO.cm.L.out", "resultsSIP0/pcms/results32_1CY1.cm.A.out", "resultsSIP0/pcms/results8_1A0G.cm.B.out"]
+
+pdbs0lista = ["resultsSIP0/pdbs/results16_103l.0.out", "resultsSIP0/pdbs/results32_1AMU.out", "resultsSIP0/pdbs/results8_103l.0.out", "resultsSIP0/pdbs/results16_9ldb.1.out", "resultsSIP0/pdbs/results32_1ARO.out", "resultsSIP0/pdbs/results8_9ldb.1.out"]
+pdbs1lista = ["resultsSIP1/pdbs/results16_103l.0.out", "resultsSIP1/pdbs/results32_1AMU.out", "resultsSIP1/pdbs/results8_103l.0.out", "resultsSIP1/pdbs/results16_9ldb.1.out", "resultsSIP1/pdbs/results32_1ARO.out", "resultsSIP1/pdbs/results8_9ldb.1.out"]
+
+ppigo1lista = ["resultsSIP1/ppigo/resultst1_4.3.out", "resultsSIP1/ppigo/resultst2_8.4.out", "resultsSIP1/ppigo/results8_1.6.out",  "resultsSIP1/ppigo/resultst1_4.9.out", "resultsSIP1/ppigo/resultst1_4.9.out", "resultsSIP1/ppigo/results4.6.out", "resultsSIP1/ppigo/resultst1_4.3.out", "resultsSIP1/ppigo/resultst2_8.4.out"]
+ppigo0lista = ["resultsSIP0/ppigo/resultst1_4.3.out", "resultsSIP0/ppigo/resultst2_8.4.out", "resultsSIP0/ppigo/results8_1.6.out",  "resultsSIP0/ppigo/resultst1_4.9.out", "resultsSIP0/ppigo/resultst1_4.9.out", "resultsSIP0/ppigo/results4.6.out", "resultsSIP0/ppigo/resultst1_4.3.out", "resultsSIP0/ppigo/resultst2_8.4.out"]
+
+# strings for files where we will save our stats
+
+aids0statslista = ["nodesStatsSIP0/aids/stats0.txt", "nodesStatsSIP0/aids/stats1.txt", "nodesStatsSIP0/aids/stats2.txt","nodesStatsSIP0/aids/stats3.txt","nodesStatsSIP0/aids/stats4.txt","nodesStatsSIP0/aids/stats5.txt"]
+aids1statslista = ["nodesStatsSIP1/aids/stats0.txt", "nodesStatsSIP1/aids/stats1.txt", "nodesStatsSIP1/aids/stats2.txt","nodesStatsSIP1/aids/stats3.txt","nodesStatsSIP1/aids/stats4.txt","nodesStatsSIP1/aids/stats5.txt"]
+
+pcms1statslista = ["nodesStatsSIP1/pcms/stats16_1BLH.cm.A.txt", "nodesStatsSIP1/pcms/stats32_1ATU.cm.A.txt", "nodesStatsSIP1/pcms/stats32_1D1U.cm.A.txt", "nodesStatsSIP1/pcms/stats16_1C5G.cm.A.txt", "nodesStatsSIP1/pcms/stats32_1CNQ.cm.A.txt", "nodesStatsSIP1/pcms/stats8_1A0G.cm.A.txt", "nodesStatsSIP1/pcms/stats32_1ARO.cm.L.txt", "nodesStatsSIP1/pcms/stats32_1CY1.cm.A.txt", "nodesStatsSIP1/pcms/stats8_1A0G.cm.B.txt"]
+pcms0statslista = ["nodesStatsSIP0/pcms/stats16_1BLH.cm.A.txt", "nodesStatsSIP0/pcms/stats32_1ATU.cm.A.txt", "nodesStatsSIP0/pcms/stats32_1D1U.cm.A.txt", "nodesStatsSIP0/pcms/stats16_1C5G.cm.A.txt", "nodesStatsSIP0/pcms/stats32_1CNQ.cm.A.txt", "nodesStatsSIP0/pcms/stats8_1A0G.cm.A.txt", "nodesStatsSIP0/pcms/stats32_1ARO.cm.L.txt", "nodesStatsSIP0/pcms/stats32_1CY1.cm.A.txt", "nodesStatsSIP0/pcms/stats8_1A0G.cm.B.txt"]
+
+pdbs0statslista = ["nodesStatsSIP0/pdbs/stats16_103l.0.txt", "nodesStatsSIP0/pdbs/stats32_1AMU.txt", "nodesStatsSIP0/pdbs/stats8_103l.0.txt", "nodesStatsSIP0/pdbs/stats16_9ldb.1.txt", "nodesStatsSIP0/pdbs/stats32_1ARO.txt", "nodesStatsSIP0/pdbs/stats8_9ldb.1.txt"]
+pdbs1statslista = ["nodesStatsSIP1/pdbs/stats16_103l.0.txt", "nodesStatsSIP1/pdbs/stats32_1AMU.txt", "nodesStatsSIP1/pdbs/stats8_103l.0.txt", "nodesStatsSIP1/pdbs/stats16_9ldb.1.txt", "nodesStatsSIP1/pdbs/stats32_1ARO.txt", "nodesStatsSIP1/pdbs/stats8_9ldb.1.txt"]
+
+ppigo1statslista = ["nodesStatsSIP1/ppigo/statst1_4.3.txt", "nodesStatsSIP1/ppigo/statst2_8.4.txt", "nodesStatsSIP1/ppigo/stats8_1.6.txt", "nodesStatsSIP1/ppigo/statst1_4.9.txt", "nodesStatsSIP1/ppigo/statst1_4.9.txt", "nodesStatsSIP1/ppigo/stats4.6.txt", "nodesStatsSIP1/ppigo/statst1_4.3.txt", "nodesStatsSIP1/ppigo/statst2_8.4.txt"]
+ppigo0statslista = ["nodesStatsSIP0/ppigo/statst1_4.3.txt", "nodesStatsSIP0/ppigo/statst2_8.4.txt", "nodesStatsSIP0/ppigo/stats8_1.6.txt",  "nodesStatsSIP0/ppigo/statst1_4.9.txt", "nodesStatsSIP0/ppigo/statst1_4.9.txt", "nodesStatsSIP0/ppigo/stats4.6.txt", "nodesStatsSIP0/ppigo/statst1_4.3.txt", "nodesStatsSIP0/ppigo/statst2_8.4.txt"]
+
+def maxNodes(fila):
+    maxi=0
+    with open(fila) as fp:
+        for line in fp.readlines():
+            linelista=line.split(";")
+            if len(linelista) > 1:
+                nodeslista = linelista[2].split(" ")
+                number = int(nodeslista[1])
+                if number > maxi:
+                    maxi = number              
+    return maxi
+
+def generateCommand(filelista,statslista):
+    n = 0
+    while(n < len(filelista)):
+        fila1=filelista[n]
+        fila2=statslista[n]
+        maxi=maxNodes(fila1)
+        for i in range(0,maxi+1):
+            if i == 0:
+                #print "echo " + str(maxi) + " > " + fila2
+                print "grep \"" + str(i) + " nodes;\" " + fila1 + " |wc -l > " + fila2
+            else:
+                print "grep \"" + str(i) + " nodes;\" " + fila1 + " |wc -l >> " + fila2
+        n=n+1
+
+
+generateCommand(aids0lista,aids0statslista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nodesStatsParser.py
@@ -0,0 +1,38 @@
+aids0statslista = ["nodesStatsSIP0/aids/stats0.txt", "nodesStatsSIP0/aids/stats1.txt", "nodesStatsSIP0/aids/stats2.txt","nodesStatsSIP0/aids/stats3.txt","nodesStatsSIP0/aids/stats4.txt","nodesStatsSIP0/aids/stats5.txt"]
+aids1statslista = ["nodesStatsSIP1/aids/stats0.txt", "nodesStatsSIP1/aids/stats1.txt", "nodesStatsSIP1/aids/stats2.txt","nodesStatsSIP1/aids/stats3.txt","nodesStatsSIP1/aids/stats4.txt","nodesStatsSIP1/aids/stats5.txt"]
+
+pcms1statslista = ["nodesStatsSIP1/pcms/stats16_1BLH.cm.A.txt", "nodesStatsSIP1/pcms/stats32_1ATU.cm.A.txt", "nodesStatsSIP1/pcms/stats32_1D1U.cm.A.txt", "nodesStatsSIP1/pcms/stats16_1C5G.cm.A.txt", "nodesStatsSIP1/pcms/stats32_1CNQ.cm.A.txt", "nodesStatsSIP1/pcms/stats8_1A0G.cm.A.txt", "nodesStatsSIP1/pcms/stats32_1ARO.cm.L.txt", "nodesStatsSIP1/pcms/stats32_1CY1.cm.A.txt", "nodesStatsSIP1/pcms/stats8_1A0G.cm.B.txt"]
+pcms0statslista = ["nodesStatsSIP0/pcms/stats16_1BLH.cm.A.txt", "nodesStatsSIP0/pcms/stats32_1ATU.cm.A.txt", "nodesStatsSIP0/pcms/stats32_1D1U.cm.A.txt", "nodesStatsSIP0/pcms/stats16_1C5G.cm.A.txt", "nodesStatsSIP0/pcms/stats32_1CNQ.cm.A.txt", "nodesStatsSIP0/pcms/stats8_1A0G.cm.A.txt", "nodesStatsSIP0/pcms/stats32_1ARO.cm.L.txt", "nodesStatsSIP0/pcms/stats32_1CY1.cm.A.txt", "nodesStatsSIP0/pcms/stats8_1A0G.cm.B.txt"]
+
+pdbs0statslista = ["nodesStatsSIP0/pdbs/stats16_103l.0.txt", "nodesStatsSIP0/pdbs/stats32_1AMU.txt", "nodesStatsSIP0/pdbs/stats8_103l.0.txt", "nodesStatsSIP0/pdbs/stats16_9ldb.1.txt", "nodesStatsSIP0/pdbs/stats32_1ARO.txt", "nodesStatsSIP0/pdbs/stats8_9ldb.1.txt"]
+pdbs1statslista = ["nodesStatsSIP1/pdbs/stats16_103l.0.txt", "nodesStatsSIP1/pdbs/stats32_1AMU.txt", "nodesStatsSIP1/pdbs/stats8_103l.0.txt", "nodesStatsSIP1/pdbs/stats16_9ldb.1.txt", "nodesStatsSIP1/pdbs/stats32_1ARO.txt", "nodesStatsSIP1/pdbs/stats8_9ldb.1.txt"]
+
+ppigo1statslista = ["nodesStatsSIP1/ppigo/statst1_4.3.txt", "nodesStatsSIP1/ppigo/statst2_8.4.txt", "nodesStatsSIP1/ppigo/stats8_1.6.txt", "nodesStatsSIP1/ppigo/statst1_4.9.txt", "nodesStatsSIP1/ppigo/statst1_4.9.txt", "nodesStatsSIP1/ppigo/stats4.6.txt", "nodesStatsSIP1/ppigo/statst1_4.3.txt", "nodesStatsSIP1/ppigo/statst2_8.4.txt"]
+ppigo0statslista = ["nodesStatsSIP0/ppigo/statst1_4.3.txt", "nodesStatsSIP0/ppigo/statst2_8.4.txt", "nodesStatsSIP0/ppigo/stats8_1.6.txt",  "nodesStatsSIP0/ppigo/statst1_4.9.txt", "nodesStatsSIP0/ppigo/statst1_4.9.txt", "nodesStatsSIP0/ppigo/stats4.6.txt", "nodesStatsSIP0/ppigo/statst1_4.3.txt", "nodesStatsSIP0/ppigo/statst2_8.4.txt"]
+
+alles=[]
+
+
+def parseStats(fila):
+    maxi=0
+    smallalles=[]
+    with open(fila) as fp:
+        for line in fp.readlines():
+                maxi=maxi+1
+                number = line.strip('\n')
+                smallalles.append(int(number))
+    return smallalles
+
+def generatePlotableStats(aids0statslista):
+    for fila in aids0statslista:    
+        alles.append(parseStats(fila))
+    return alles
+
+generatePlotableStats(aids0statslista)
+print alles
+
+
+
+
+
+
